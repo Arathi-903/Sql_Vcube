c7276a049fd3dd9e2481588b586e54b97e0784a6
Arathi-903 <aarathijenne583@gmail.com>
2026-06-02 11:24:57 +0530

    Add files via upload

---
+++ Department_Table.sql
@@ -0,0 +1,10 @@
+use  food_delivery_db;
+Create table Department(
+Department_Id int primary key,
+Department_Name varchar(255) not null
+);
+select * from Department;
+insert into Department values(5,"ECE"),
+(2,"ECE"),
+(3,"ECE"),
+(4,"ECE");
